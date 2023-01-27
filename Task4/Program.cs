954c03115efbaac3e3040698cae5896a75f4e624
AlbulOlya <albul.olya@gmail.com>
2023-01-27 20:52:19 +0500

    Task 4 solved

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = int.Parse(Console.ReadLine());
+if (numberA % 2 ==0)
+{
+    Console.Write(numberA);
+    Console.WriteLine(" четное число");
+}
+else
+{
+    Console.Write(numberA);
+    Console.WriteLine(" нечетное число");
+}
