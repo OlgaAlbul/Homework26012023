c27f1637de923a8bccb425144fb46d5561aef35d
AlbulOlya <albul.olya@gmail.com>
2023-01-27 21:20:04 +0500

    Task 5 solved

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = 1;
+while (numberB <= numberA)
+{
+    if (numberB % 2 == 0)
+    {
+        Console.WriteLine(numberB);
+        numberB = numberB+1;
+    }
+    else
+    {
+        numberB = numberB+1;
+    }
+}
