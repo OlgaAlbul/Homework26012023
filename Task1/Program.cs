dec7d692d68aa4a0f93d57ba285781e0ac7c03a2
AlbulOlya <albul.olya@gmail.com>
2023-01-27 19:45:47 +0500

    Task 1 solved

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA ==numberB*numberB)
+{
+    Console.WriteLine("Первое число является квадратом втрого");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
