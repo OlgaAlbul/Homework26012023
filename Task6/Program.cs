b1ef4797ce8eda5676099e3328d8d01463a65dd4
AlbulOlya <albul.olya@gmail.com>
2023-01-27 21:26:27 +0500

    Task 6 solved

---
+++ Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = 0;
+numberB = numberA/10;
+numberA = numberA-numberB*10;
+Console.WriteLine(numberA);
