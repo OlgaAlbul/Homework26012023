37610a790e6a511f237879222ff59e286ae9ae68
AlbulOlya <albul.olya@gmail.com>
2023-01-27 22:11:04 +0500

    Task 8 solved

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = new Random().Next(10,99);
+Console.WriteLine(numberA);
+int numberD = 0;
+int numberU = 0;
+numberD = numberA/10;
+numberU = numberA-numberD*10;
+if (numberD>numberU)
+{
+    Console.Write("Наибольшая цифра числа ");
+    Console.WriteLine(numberD);
+}
+else
+{
+    Console.Write("Наибольшая цифра числа ");
+    Console.WriteLine(numberU);
+}
