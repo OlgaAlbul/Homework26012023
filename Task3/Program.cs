07b3daf47d74e049f2556617e49ffc288fb00499
AlbulOlya <albul.olya@gmail.com>
2023-01-27 20:43:16 +0500

    Task 3 solved

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿int numberA = new Random().Next(1,100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,100);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1,100);
+Console.WriteLine(numberC);
+if (numberA > numberB)
+{
+    if (numberA > numberC)
+    {
+        Console.Write("Наибольшее число ");
+        Console.WriteLine(numberA);
+    }
+    else if (numberC > numberB)
+    {
+        Console.Write("Наибольшее число ");
+        Console.WriteLine(numberC);
+    }
+    else 
+    {
+        Console.Write("Наибольшее число ");
+        Console.WriteLine(numberB);
+    }
+}
+else if (numberB > numberC)
+{
+    Console.Write("Наибольшее число ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.Write("Наибольшее число ");
+    Console.WriteLine(numberC);
+}
