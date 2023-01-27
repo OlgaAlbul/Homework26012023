4caa32b7d95576f786cce165bbe6ef161d307427
AlbulOlya <albul.olya@gmail.com>
2023-01-27 20:18:36 +0500

    Task 2 solved

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int numberA = int.Parse(Console.ReadLine());
+if (numberA == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (numberA == 2)
+{
+   Console.WriteLine("Вторник"); 
+}
+else if (numberA == 3)
+{
+   Console.WriteLine("Среда");  
+}
+else if (numberA == 4)
+{
+   Console.WriteLine("Четверг"); 
+}
+else if (numberA == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (numberA == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (numberA == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
