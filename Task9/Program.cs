5c6846979e580cf913e1ea67a5071a07d53f3ed6
AlbulOlya <albul.olya@gmail.com>
2023-01-27 22:19:42 +0500

    Task 9 solved

---
+++ Task9/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int numberA = int.Parse(Console.ReadLine());
+int numberH = 0;
+int numberD = 0;
+int numberU = 0;
+numberH = numberA/100;
+numberD = numberA/10;
+numberU = numberA-numberD*10;
+Console.WriteLine(numberH*10+numberU);
