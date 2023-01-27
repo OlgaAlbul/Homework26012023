2baface3283c02089c325980d200aab46ee4f157
AlbulOlya <albul.olya@gmail.com>
2023-01-27 22:36:48 +0500

    Task 10 solved

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = new Random().Next(1,100);
+Console.WriteLine(numberA);
+Console.WriteLine("Введите число ");
+int numberB = int.Parse(Console.ReadLine());
+int numberS = numberA % numberB;
+if (numberS == 0)
+{
+    Console.Write("Число ");
+    Console.Write(numberA);
+    Console.Write(" кратно числу ");
+    Console.Write(numberB);
+}
+else
+{
+    Console.WriteLine(numberS);
+}
