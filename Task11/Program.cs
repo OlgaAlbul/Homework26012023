9a8a3207d7466d0db87d98a42cd1614ca2b1af61
AlbulOlya <albul.olya@gmail.com>
2023-01-28 18:41:43 +0500

    Task 11 solved

---
+++ Task11/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int numberA = int.Parse(Console.ReadLine());
+if(numberA > 99)
+{
+    Console.WriteLine((numberA / 100) % 10);
+}
+else
+{
+    Console.WriteLine("Отсутствует третья цифра");
+}
