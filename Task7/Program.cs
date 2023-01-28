2e01d940568686d5e3f2f66c58b3bf06ac5d1e73
AlbulOlya <albul.olya@gmail.com>
2023-01-28 18:47:26 +0500

    Task 7 new solution

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,8 +1,3 @@
 ﻿Console.WriteLine("Введите трехзначное число ");
 int numberA = int.Parse(Console.ReadLine());
-int numberH = 0;
-int numberD = 0;
-numberH = numberA/100;
-numberA = numberA - numberH*100;
-numberD = numberA/10;
-Console.WriteLine(numberD);
+Console.WriteLine(numberA%100/10);
